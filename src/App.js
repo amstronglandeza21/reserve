cced2eb000470fa0e1d09bf1b1e090dd25d54453
Luiese Amstrong <amstronglandeza21@gmail.com>
2023-11-13 15:41:54 +0800

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import ReservationForm from './ReservationForm';
-import ThankYouPage from 'https://amstronglandeza21.github.io/ThankYou'; // Create ThankYouPage component
+import ThankYouPage from '/ThankYou'; // Create ThankYouPage component
 
 const App = () => {
   return (
