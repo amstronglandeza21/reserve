a90e0242222b43ce5713ca11ceb10916812053a3
Luiese Amstrong <amstronglandeza21@gmail.com>
2024-01-16 16:36:40 +0800

    Initial Commit

--- src/Navbar.js
+++ src/Navbar.js
@@ -6,7 +6,7 @@ const Navbar = () => {
   return (
     <nav>
     <a className="navbar-brand" href="https://reservenow.online">
-      <img src="https://drive.google.com/uc?id=1AZ-d0WyBy-8C0zbqAyw_DhoCtN3pXZCD" width="195" height="45" alt="trimexcolleges" />
+      <img src="https://drive.google.com/uc?id=1vPsJ2qfxAGmlns7oWDbYNk3X6LJP8d7A" width="195" height="45" alt="trimexcolleges" />
     </a>
       {/* Add additional navigation elements as needed */}
     </nav>
